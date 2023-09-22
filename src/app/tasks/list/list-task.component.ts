18125ec89097a64cdd7ee4ade327e5b63ac80fbd
Morgan <morgan.espindola@gmail.com>
2023-09-21 22:54:20 -0300

    Added task list dynamic

--- src/app/tasks/list/list-task.component.ts
+++ src/app/tasks/list/list-task.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task, TaskService } from '../shared';
 
 @Component({
   selector: 'app-list-task',
   templateUrl: './list-task.component.html',
   styleUrls: ['./list-task.component.scss'],
 })
-export class ListTaskComponent {}
+export class ListTaskComponent implements OnInit {
+  tasks: Task[];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.updateTasks();
+  }
+
+  updateTasks(): void {
+    this.tasks = this.taskService.getAllTasks();
+  }
+}
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ListTaskComponent } from './list';
 import { TaskService } from './shared';
 
 @NgModule({
   declarations: [ListTaskComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   providers: [TaskService],
 })
 export class TasksModule {}
