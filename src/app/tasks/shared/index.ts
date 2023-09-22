dd95f2f4e38abeb3be71b7a04f7ca6e0c94361ef
Morgan <morgan.espindola@gmail.com>
2023-09-21 22:59:33 -0300

    Generated Create Task Component

--- src/app/tasks/shared/index.ts
+++ src/app/tasks/shared/index.ts
@@ -1,4 +1,5 @@
+export * from '../create';
 export * from '../list';
 export * from './task.model';
 export * from './task.service';
-export * from './tasks-routing.module'
+export * from './tasks-routing.module';
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CreateTaskComponent } from './create/';
 import { ListTaskComponent } from './list';
 import { TaskService } from './shared';
 
 @NgModule({
-  declarations: [ListTaskComponent],
+  declarations: [ListTaskComponent, CreateTaskComponent],
   imports: [CommonModule, FormsModule],
   providers: [TaskService],
 })
