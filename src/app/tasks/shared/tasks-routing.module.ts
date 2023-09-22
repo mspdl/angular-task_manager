1c2e4f123cc02469525a69edf7907b2dc677d595
Morgan <morgan.espindola@gmail.com>
2023-09-21 23:03:26 -0300

    Added route to create task

--- src/app/tasks/shared/tasks-routing.module.ts
+++ src/app/tasks/shared/tasks-routing.module.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { CreateTaskComponent } from '../create';
 import { ListTaskComponent } from '../list';
 
 export const TaskRoutes: Routes = [
   { path: 'tasks', redirectTo: 'tasks/list' },
   { path: 'tasks/list', component: ListTaskComponent },
+  { path: 'tasks/add', component: CreateTaskComponent },
 ];
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { CreateTaskComponent } from './create/';
 import { ListTaskComponent } from './list';
 import { TaskService } from './shared';
 
 @NgModule({
   declarations: [ListTaskComponent, CreateTaskComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   providers: [TaskService],
 })
 export class TasksModule {}
