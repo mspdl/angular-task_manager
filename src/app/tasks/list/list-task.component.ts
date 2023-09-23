58f7703e6819ce8feaf791853a843e2677a30913
Morgan <morgan.espindola@gmail.com>
2023-09-23 19:27:23 -0300

    Remove Task

--- src/app/tasks/list/list-task.component.ts
+++ src/app/tasks/list/list-task.component.ts
@@ -18,4 +18,12 @@ export class ListTaskComponent implements OnInit {
   updateTasks(): void {
     this.tasks = this.taskService.getAllTasks();
   }
+
+  onRemoveTask($event: MouseEvent, task: Task): void {
+    $event.preventDefault();
+    if (confirm(`Do you want to remove the task ${task.name}?`)) {
+      this.taskService.removeTask(task.id);
+      this.updateTasks();
+    }
+  }
 }
